e44b0c7d275a7f387613b43af068488e018e199e
bharaths44 <bharathsatheesan@gmail.com>
2024-01-06 20:16:30 +0530

    Added pgm1 using C

---
+++ Exp01/pgm1.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+typedef struct
+{
+    char type[100];
+    char value[100];
+    int lineNumber;
+} Token;
+
+void printTableHeader()
+{
+    printf("| %-15s | %-15s | %-15s |\n", "Lexeme", "Token", "Line Number");
+    printf("|-----------------|-----------------|-----------------|\n");
+}
+
+void printToken(Token token)
+{
+    printf("| %-15s | %-15s | %-15d |\n", token.value, token.type, token.lineNumber);
+}
+
+int isOperator(char c)
+{
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '=' || c == '<' || c == '>' || c == '!' || c == '&' || c == '|');
+}
+
+int main()
+{
+    FILE *file = fopen("input_1.txt", "r");
+    if (file == NULL)
+    {
+        perror("Error opening file");
+        return 1;
+    }
+
+    char currentChar;
+    char buffer[100];
+    int bufferIndex = 0;
+    int lineNumber = 1;
+
+    printTableHeader();
+
+    while ((currentChar = fgetc(file)) != EOF)
+    {
+        if (isspace(currentChar))
+        {
+            // Ignore redundant spaces, tabs, and new lines
+            if (currentChar == '\n')
+            {
+                lineNumber++;
+            }
+            continue;
+        }
+        else if (isalnum(currentChar))
+        {
+            // Collect alphanumeric characters into the buffer
+            buffer[bufferIndex++] = currentChar;
+
+            // Check if the next character is alphanumeric
+            while (isalnum(currentChar = fgetc(file)) || currentChar == '_')
+            {
+                buffer[bufferIndex++] = currentChar;
+            }
+
+            // Null-terminate the buffer
+            buffer[bufferIndex] = '\0';
+
+            // Determine the token type
+            Token token;
+            token.lineNumber = lineNumber;
+            if (isdigit(buffer[0]))
+            {
+                strcpy(token.type, "Number");
+            }
+            else
+            {
+                strcpy(token.type, "Identifier");
+            }
+            // Copy the value to the token
+            strcpy(token.value, buffer);
+
+            // Print the token
+            printToken(token);
+
+            // Reset the buffer index
+            bufferIndex = 0;
+
+            // Unget the last character to avoid skipping it
+            ungetc(currentChar, file);
+        }
+        else if (isOperator(currentChar))
+        {
+            // Operators are treated as single-character tokens
+            Token token;
+            token.lineNumber = lineNumber;
+            strcpy(token.type, "Operator");
+            token.value[0] = currentChar;
+            token.value[1] = '\0';
+
+            // Print the token
+            printToken(token);
+        }
+        else
+        {
+            // Other characters are treated as single-character tokens
+            Token token;
+            token.lineNumber = lineNumber;
+            strcpy(token.type, "Other");
+            token.value[0] = currentChar;
+            token.value[1] = '\0';
+
+            // Print the token
+            printToken(token);
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
