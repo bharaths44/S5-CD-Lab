ca611fa1363ebbd825c6ac33cccb1d67277d48bb
bharaths44 <bharathsatheesan@gmail.com>
2024-02-04 23:53:42 +0530

    Fix grammar rules in check_expr.l and pgm8.c

--- Exp08/pgm8.c
+++ Exp08/pgm8.c
@@ -5,18 +5,25 @@
 #define FAILED 0
 const char *cursor;
 char string[64];
-int E(const char *E_rule, const char *Edash_rule, const char *T_rule, const char *Tdash_rule, const char *F_rule);
-int Edash(const char *Edash_rule, const char *T_rule, const char *Tdash_rule, const char *F_rule);
-int T(const char *T_rule, const char *Tdash_rule, const char *F_rule);
-int Tdash(const char *Tdash_rule, const char *F_rule);
-int F(const char *F_rule);
+char E_rule[] = "TE'";
+char Edash_rule[] = "+TE`|$";
+char T_rule[] = "FT`";
+char Tdash_rule[] = "*FT`|$";
+char F_rule[] = "(E)|i";
+
+int E();
+int Edash();
+int T();
+int Tdash();
+int F();
+
 // Grammar rule: E -> T E'
-int E(const char *E_rule, const char *Edash_rule, const char *T_rule, const char *Tdash_rule, const char *F_rule)
+int E()
 {
     printf("%-16s E->%s\n", cursor, E_rule);
-    if (T(T_rule, Tdash_rule, F_rule))
-    {                                                      // Call non-terminal T
-        if (Edash(Edash_rule, T_rule, Tdash_rule, F_rule)) // Call non-terminal E'
+    if (T())
+    {                // Call non-terminal T
+        if (Edash()) // Call non-terminal E'
             return SUCCESS;
         else
             return FAILED;
@@ -26,15 +33,15 @@ int E(const char *E_rule, const char *Edash_rule, const char *T_rule, const char
 }
 
 // Grammar rule: E' -> + T E' | $
-int Edash(const char *Edash_rule, const char *T_rule, const char *Tdash_rule, const char *F_rule)
+int Edash()
 {
     if (*cursor == '+')
     {
         printf("%-16s E'->%s\n", cursor, Edash_rule);
         cursor++;
-        if (T(T_rule, Tdash_rule, F_rule))
-        {                                                      // Call non-terminal T
-            if (Edash(Edash_rule, T_rule, Tdash_rule, F_rule)) // Call non-terminal E'
+        if (T())
+        {                // Call non-terminal T
+            if (Edash()) // Call non-terminal E'
                 return SUCCESS;
             else
                 return FAILED;
@@ -50,12 +57,12 @@ int Edash(const char *Edash_rule, const char *T_rule, const char *Tdash_rule, co
 }
 
 // Grammar rule: T -> F T'
-int T(const char *T_rule, const char *Tdash_rule, const char *F_rule)
+int T()
 {
     printf("%-16s T->%s\n", cursor, T_rule);
-    if (F(F_rule))
-    {                                  // Call non-terminal F
-        if (Tdash(Tdash_rule, F_rule)) // Call non-terminal T'
+    if (F())
+    {                // Call non-terminal F
+        if (Tdash()) // Call non-terminal T'
             return SUCCESS;
         else
             return FAILED;
@@ -65,15 +72,15 @@ int T(const char *T_rule, const char *Tdash_rule, const char *F_rule)
 }
 
 // Grammar rule: T' -> * F T' | $
-int Tdash(const char *Tdash_rule, const char *F_rule)
+int Tdash()
 {
     if (*cursor == '*')
     {
         printf("%-16s T'->%s\n", cursor, Tdash_rule);
         cursor++;
-        if (F(F_rule))
-        {                                  // Call non-terminal F
-            if (Tdash(Tdash_rule, F_rule)) // Call non-terminal T'
+        if (F())
+        {                // Call non-terminal F
+            if (Tdash()) // Call non-terminal T'
                 return SUCCESS;
             else
                 return FAILED;
@@ -89,13 +96,13 @@ int Tdash(const char *Tdash_rule, const char *F_rule)
 }
 
 // Grammar rule: F -> ( E ) | i
-int F(const char *F_rule)
+int F()
 {
     if (*cursor == '(')
     {
         printf("%-16s F->%s\n", cursor, F_rule);
         cursor++;
-        if (E(F_rule, F_rule, F_rule, F_rule, F_rule))
+        if (E())
         { // Call non-terminal E
             if (*cursor == ')')
             {
@@ -118,27 +125,9 @@ int F(const char *F_rule)
         return FAILED;
 }
 
-// Function to input production rule
-// void inputProduction(char *non_terminal, char *production_rule)
-// {
-//     printf("Enter production rule for %s: ", non_terminal);
-//     scanf("%s", production_rule);
-// }
-
 int main()
 {
 
-    // inputProduction("E", E_rule);
-    // inputProduction("E'", Edash_rule);
-    // inputProduction("T", T_rule);
-    // inputProduction("T'", Tdash_rule);
-    // inputProduction("F", F_rule);
-    char E_rule[] = "TE'";
-
-    char Edash_rule[] = "+TE`|$";
-    char T_rule[] = "FT`";
-    char Tdash_rule[] = "*FT`|$";
-    char F_rule[] = "(E)|i";
     puts("Enter the string");
     scanf("%s", string); // Read input from the user
     cursor = string;
@@ -147,7 +136,7 @@ int main()
     puts("--------------------------------");
 
     // Call the starting non-terminal E
-    if (E(E_rule, Edash_rule, T_rule, Tdash_rule, F_rule) && *cursor == '\0')
+    if (E() && *cursor == '\0')
     { // If parsing is successful and the cursor has reached the end
         puts("--------------------------------");
         puts("String is successfully parsed");
